06620cb10de0c9e6445288043994f90ec7d0cf9c
edinhorod <edinho_rodrigues@hotmail.com>
2021-04-20 19:38:33 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { SignInnButton } from '../SignInButton';
+
+import styles from './styles.module.scss';
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="IG News"/>
+                <nav>
+                    <a className={styles.active} href="">Home</a>
+                    <a href="">Posts</a>
+                </nav>
+
+                <SignInnButton />
+            </div>
+        </header>
+    );
+}
